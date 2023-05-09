551d0c6a50aebb2c130849efa9f25b2af43828e6
chrillah <ch.ronnberg@gmail.com>
2023-05-09 19:52:58 +0200

    it worked

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ import express from 'express'
 
 const app = express()
 
-const client = new MongoClient('mongodb://localhost:27017/'),
+const client = new MongoClient('mongodb://127.0.0.1:27017/test'),
 users = client.db('test').collection('users')
 
 
 
 
-await users.insertOne({
-    address: {
-      city: 'Köpenhamn',
-      street: 'Fulgatan 1',
-      zipCode: 12345
-    },
-    firstName: 'Johanna',
-    registered: new Date('2023-01-14T15:41:20.000Z'),
-    surname: 'Dopesson'
-  })
+// await users.insertOne({
+//     address: {
+//       city: 'Köpenhamn',
+//       street: 'Fulgatan 1',
+//       zipCode: 12345
+//     },
+//     firstName: 'Johanna',
+//     registered: new Date('2023-01-14T15:41:20.000Z'),
+//     surname: 'Dopesson'
+//   })
 
-  console.log('Dokumentet har lagts in!')
+//   console.log('Dokumentet har lagts in!')
 
 
-// app.get('/', async (request, response) =>{
-//     response.send(await users.find().toArray())
-// })
+app.get('/', async (request, response) =>{
+    response.send(await users.find().toArray())
+})
 
 // app.get('/', (req, res)=>{
 //     res.send('FUCK')
 // })
 
-// app.listen(8080, ()=>{
-//     console.log('Its On')
-// })
+app.listen(8080, ()=>{
+    console.log('Its On')
+})
 
 // await users.insertOne({
 //     address : {
